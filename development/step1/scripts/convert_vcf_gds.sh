3bfef6862432e1af84f65cfbf3c4d4baba4a9d67
Anastasia Gurinovich <gurinovichn@yahoo.com>
2019-07-16 10:58:34 -0400

    Created phenotype file

--- development/step1/scripts/convert_vcf_gds.sh
+++ development/step1/scripts/convert_vcf_gds.sh
@@ -5,6 +5,7 @@ module load gcc/7.2.0
 
 vcf_file=$1
 gds_file=$2
+pheno_file=$3
 
 #convert VCF files to GDS files
 for chr in $(seq 1 22); do
@@ -15,3 +16,6 @@ wait
 
 #combine gds files per chromosome into one file
 Rscript combine_gds.R $vcf_file $gds_file
+
+#PCA_AiR step
+Rscript PCA_AiR.R $gds_file $pheno_file
