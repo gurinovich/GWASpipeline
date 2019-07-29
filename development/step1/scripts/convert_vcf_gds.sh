432eedd571d5aeaf70d8bd5b45de614a4c0bd1d2
Zeyuan Song <zeyuan@bu.edu>
2019-07-29 18:22:50 -0400

    fixed error in summary step

--- development/step1/scripts/convert_vcf_gds.sh
+++ development/step1/scripts/convert_vcf_gds.sh
@@ -41,11 +41,11 @@ done
 wait
 
 #summary
-find ./results -maxdepth 1 -name '*.csv' | xargs -n 1 tail -n +2 | awk -F ',' '{print $2 \",\" $3 \",\" $5 \",\" $6 \",\" $7 \",\" $8 \",\" $9 \",\" $10}'> ${result_file}".csv"
+find ./results -maxdepth 1 -name '*.csv' | xargs -n 1 tail -n +2 | awk -F ',' '{print $2 \",\" $3 \",\" $5 \",\" $6 \",\" $7 \",\" $8 \",\" $9 \",\" $10}' > ${result_file}".csv"
 
 wait
 
-sed -i '1i chr,pos,n.obs,freq,Score,Score.SE,Score.Stat,Score.pval' ${result_file}".csv"))
+sed -i '1i chr,pos,n.obs,freq,Score,Score.SE,Score.Stat,Score.pval' ${result_file}".csv"
 
 wait
 
