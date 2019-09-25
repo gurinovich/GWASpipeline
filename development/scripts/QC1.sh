82c541ba45fd9ef158e1c03c93fa5c7765925f04
Anastasia Gurinovich <gurinovichn@yahoo.com>
2019-09-25 14:09:10 -0400

    updated QC

--- development/scripts/QC1.sh
+++ development/scripts/QC1.sh
@@ -2,10 +2,8 @@
 
 vcf_file=$1".vcf.gz"
 
-#only keep variants that have been successfully genotyped in 50% of individuals and remove SNPs not in HWE & remove SNPs with missing rate > 1% & remove "monomorphic" SNPs - the ones that have no alternative
-vcftools --gzvcf $vcf_file --max-missing 0.5 --hwe 0.000001 --max-missing 0.01 --recode --stdout | gzip -c > $1"_qc.vcf.gz"
-
-
+# remove SNPs with < 1% missing rate
+vcftools --gzvcf $vcf_file --max-missing 0.99 --recode --stdout | gzip -c > $1"_qc.vcf.gz"
 
 # remove monomorphic SNPs:
 bcftools view -e 'COUNT(GT="AA")=N_SAMPLES || COUNT(GT="RR")=N_SAMPLES || COUNT(GT="AR")=N_SAMPLES || COUNT(GT="RA")=N_SAMPLES' $1"_qc.vcf.gz" -Oz -o $1"_qc.vcf.gz"
