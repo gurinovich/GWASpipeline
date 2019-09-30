552d3ef04b269ef35d31d4f68721dbf571eeb947
Anastasia Gurinovich <agurinov@bu.edu>
2019-09-30 14:10:59 -0400

    Update QC1.sh

--- development/scripts/QC1.sh
+++ development/scripts/QC1.sh
@@ -5,6 +5,8 @@ vcf_file=$1".vcf.gz"
 # remove SNPs with < 1% missing rate
 vcftools --gzvcf $vcf_file --max-missing 0.99 --recode --stdout | gzip -c > $1"_qc.vcf.gz"
 
+wait
+
 # remove monomorphic SNPs:
 bcftools view -e 'COUNT(GT="AA")=N_SAMPLES || COUNT(GT="RR")=N_SAMPLES || COUNT(GT="AR")=N_SAMPLES || COUNT(GT="RA")=N_SAMPLES' $1"_qc.vcf.gz" -Oz -o $1"_qc2.vcf.gz"
 
