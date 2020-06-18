efd9b9010b859186579b93e411ced34c04688183
ZeyuanSong <51000742+ZeyuanSong@users.noreply.github.com>
2020-06-18 13:58:22 -0400

    Update 04_nullmod.R

--- pipeline/scripts/04_nullmod.R
+++ pipeline/scripts/04_nullmod.R
@@ -9,38 +9,20 @@ suppressPackageStartupMessages(library(data.table))
 
 args = commandArgs(trailingOnly=TRUE)
 gds.file <- args[1]
-pheno.file <- args[2]
-phenotypes <- args[3]
-covariates <- unlist(strsplit(args[4], ","))
-model <- args[5]
-pc_df <- args[6]
-grm <- args[7]
+phenotypes <- args[2]
+covariates <- unlist(strsplit(args[3], ","))
+model <- args[4]
+analysis.sample.id <- args[5]
+annot <- args[6]
+pc_df <- args[7]
+grm <- args[8]
 
 ####Open GDS
 gds <- seqOpen(gds.file)
 
 ####Create a SeqVarData object
-pheno.dat <- read.csv(pheno.file,stringsAsFactors=F,header=T)
-pheno.dat$sample.id <- as.character(pheno.dat$ID)
-
-for(i in 1:32){
-if(sum(colnames(pheno.dat)==paste0("PC",i))==1){
-colnames(pheno.dat)[colnames(pheno.dat)==paste0("PC",i)] <- paste0("PC",i,".pheno")
-}
-}
-
-gds.sample.id <- data.frame(sample.id= seqGetData(gds, "sample.id"),stringsAsFactors=F)
-
-annot <- left_join(gds.sample.id, pheno.dat)
-
-annot <- annot[,c("sample.id",colnames(annot)[colnames(annot)%in%c(phenotypes,covariates)])]
-
-analysis.sample.id <- na.omit(annot[,c("sample.id",colnames(annot)[colnames(annot)%in%c(phenotypes,covariates)])])$sample.id
-print(paste0("number of individuals to include : ",length(analysis.sample.id)))
-
-metadata <- data.frame(labelDescription=colnames(annot),row.names=names(annot))
-annot <- AnnotatedDataFrame(annot, metadata)
-all.equal(annot$sample.id,seqGetData(gds, "sample.id"))
+analysis.sample.id <- readRDS(analysis.sample.id)
+annot <- readRDS(annot)
 seqData <- SeqVarData(gds, sampleData=annot)
 
 ####PCA
@@ -48,6 +30,7 @@ pc.df <- readRDS(pc_df)
 
 ####add PCs to sample annotation in SeqVarData object
 annot <- AnnotatedDataFrame(pc.df)
+saveRDS(annot, "annot_pc.rds")
 sampleData(seqData) <- annot
 
 ####covariance matrix from pcrelate output
