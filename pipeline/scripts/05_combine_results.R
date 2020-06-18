1ad00cdae5dbe73e3579500ee11bc6840cc4e4e7
ZeyuanSong <51000742+ZeyuanSong@users.noreply.github.com>
2020-06-18 14:00:17 -0400

    Update 05_combine_results.R

--- pipeline/scripts/05_combine_results.R
+++ pipeline/scripts/05_combine_results.R
@@ -4,26 +4,15 @@ suppressPackageStartupMessages(library(data.table))
 args = commandArgs(trailingOnly=TRUE)
 
 path1 <- args[1]
-path2 <- args[2]
 
 chr <- 1
-res <- fread(paste0(path1, "chr_", chr, ".csv"))
-caf <- fread(paste0(path2, "CAFs.chr", chr, ".csv"))
-res <- res %>%
-  inner_join(caf, by = c("chr" = "CHROM", "pos" = "POS")) %>%
-  as.tbl()
-write.csv(res, paste0("chr", chr, ".csv"), row.names = FALSE)
+res <- fread(paste0(path1, "chr_", chr, "_caf_annotated.csv"))
 result <- res
 
 #chr <- 2
 for (chr in 2:22) {
-  res <- fread(paste0(path1, "chr_", chr, ".csv"))
-  caf <- fread(paste0(path2, "CAFs.chr", chr, ".csv"))
-  res <- res %>%
-    inner_join(caf, by = c("chr" = "CHROM", "pos" = "POS")) %>%
-    as.tbl()
-  write.csv(res, paste0("chr", chr, ".csv"), row.names = FALSE)
-  results <- bind_rows(result, res)
+  res <- fread(paste0(path1, "chr_", chr, "_caf_annotated.csv"))
+  result <- bind_rows(result, res)
 }
 
-write.csv(results, paste0("all_chr", ".csv"), row.names = FALSE)
+fwrite(result, paste0("all_chr_caf_annotated.csv"), row.names = FALSE)
