68b96a1369bd4e4ad5cb1ecea890a644e00bdab8
ZeyuanSong <51000742+ZeyuanSong@users.noreply.github.com>
2020-07-06 16:21:27 -0400

    add log file

--- pipeline/scripts/03_PC_AiR.R
+++ pipeline/scripts/03_PC_AiR.R
@@ -1,3 +1,13 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+gds.file <- args[1]
+pheno.file <- args[2]
+phenotypes <- args[3]
+covariates <- unlist(strsplit(args[4], ","))
+snpset.file <- args[5]
+
+sink("pc_air.log", append=FALSE, split=TRUE)
+date()
 suppressPackageStartupMessages(library(SeqArray))
 suppressPackageStartupMessages(library(GENESIS))
 suppressPackageStartupMessages(library(Biobase))
@@ -7,19 +17,13 @@ suppressPackageStartupMessages(library(SNPRelate))
 suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(data.table))
 
-args = commandArgs(trailingOnly=TRUE)
-gds.file <- args[1]
-pheno.file <- args[2]
-phenotypes <- args[3]
-covariates <- unlist(strsplit(args[4], ","))
-snpset.file <- args[5]
-
 ####Open GDS
 gds <- seqOpen(gds.file)
 
 ####Create a SeqVarData object
 pheno.dat <- read.csv(pheno.file, stringsAsFactors=F, header=T)
 pheno.dat$sample.id <- as.character(pheno.dat[,1])
+print(paste0("number of individuals in pheno data : ",length(pheno.dat$sample.id)))
 
 for(i in 1:32){
 if(sum(colnames(pheno.dat)==paste0("PC",i))==1){
@@ -28,13 +32,14 @@ colnames(pheno.dat)[colnames(pheno.dat)==paste0("PC",i)] <- paste0("PC",i,".phen
 }
 
 gds.sample.id <- data.frame(sample.id=seqGetData(gds, "sample.id"),stringsAsFactors=F)
+print(paste0("number of individuals in geno data : ", dim(gds.sample.id)[1]))
 
 annot <- left_join(gds.sample.id, pheno.dat)
 
 annot <- annot[,c("sample.id",colnames(annot)[colnames(annot)%in%c(phenotypes,covariates)])]
 
 analysis.sample.id <- na.omit(annot[,c("sample.id",colnames(annot)[colnames(annot)%in%c(phenotypes,covariates)])])$sample.id
-print(paste0("number of individuals to include : ",length(analysis.sample.id)))
+print(paste0("number of individuals to analyze : ",length(analysis.sample.id)))
 saveRDS(analysis.sample.id, "analysis.sample.id.rds")
 
 metadata <- data.frame(labelDescription=colnames(annot),row.names=names(annot))
@@ -60,19 +65,25 @@ if(is.na(snpset.file)){
   pruned <- unlist(snp.intersect.dat$variant.id)
   saveRDS(pruned, "pruned.rds")
 }
+print(paste0("number of variants after pruning : ",length(pruned)))
 
 ####KING
-king <- snpgdsIBDKING(gds, sample.id=analysis.sample.id, snp.id=pruned, verbose=FALSE)
+cat("\n####snpgdsIBDKING starts\n")
+king <- snpgdsIBDKING(gds, sample.id=analysis.sample.id, snp.id=pruned, verbose=T)
 kingMat <- king$kinship
 dimnames(kingMat) <- list(king$sample.id, king$sample.id)
 saveRDS(king,"king.rds")
+cat("####snpgdsIBDKING ends\n\n")
 
 ####PC-AiR
+cat("\n####pcair starts\n")
 pcs <- pcair(seqData, kinobj=kingMat, kin.thresh=2^(-11/2),
                       divobj=kingMat, div.thresh=-2^(-11/2),
              sample.include=analysis.sample.id,
-             snp.include=pruned)
+             snp.include=pruned,
+             verbose=T)
 saveRDS(pcs,"pcs.rds")
+cat("####pcair ends\n\n")
 
 pc.df <- as.data.frame(pcs$vectors)
 names(pc.df) <- paste0("PC", 1:ncol(pcs$vectors))
@@ -80,6 +91,7 @@ pc.df$sample.id <- row.names(pcs$vectors)
 pc.df <- left_join(pData(annot), pc.df, by="sample.id")
 saveRDS(pc.df,"pc.df.rds")
 
+cat("\n####PCA plot starts\n")
 png("PC1vsPC2.png")
 ggplot(pc.df, aes(PC1, PC2)) + geom_point()
 dev.off()
@@ -87,7 +99,7 @@ dev.off()
 png("PC3vsPC4.png")
 ggplot(pc.df, aes(PC3, PC4)) + geom_point()
 dev.off()
+cat("####PCA pot ends\n\n")
 
-fileConn<-file("pca_air.log")
-writeLines( paste0("number of individuals to include : ",length(analysis.sample.id)) , fileConn)
-close(fileConn)
+date()
+sink()
