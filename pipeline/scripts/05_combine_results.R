bcf91d2983b1ffb109a47189a4fc21427e9f8861
ZeyuanSong <51000742+ZeyuanSong@users.noreply.github.com>
2020-07-06 16:23:56 -0400

    add log file

--- pipeline/scripts/05_combine_results.R
+++ pipeline/scripts/05_combine_results.R
@@ -1,9 +1,11 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+path1 <- args[1]
+
+sink('combine_results.log', append=FALSE, split=TRUE)
+date()
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(data.table))
-
-args = commandArgs(trailingOnly=TRUE)
-
-path1 <- args[1]
 
 chr <- 1
 res <- fread(paste0(path1, "chr_", chr, "_caf_annotated.csv"))
@@ -16,3 +18,6 @@ for (chr in 2:22) {
 }
 
 fwrite(result, paste0("all_chr_caf_annotated.csv"), row.names = FALSE)
+
+date()
+sink()
