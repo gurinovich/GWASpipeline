9ac3643cdf00b693ff88f90fcb3afc06f61d2474
ZeyuanSong <51000742+ZeyuanSong@users.noreply.github.com>
2020-07-06 16:20:25 -0400

    add log file

--- pipeline/scripts/02_merge_gds.R
+++ pipeline/scripts/02_merge_gds.R
@@ -1,6 +1,11 @@
 #!/usr/bin/env Rscript
+sink("merge_gds.log", append=FALSE, split=TRUE)
+date()
 suppressPackageStartupMessages(library(SeqArray))
 
-args <- commandArgs(trailingOnly=TRUE)
-
+cat("\n####seqMerge starts\n")
 seqMerge(paste0("chr_",1:22,".gds"), "merged.gds")
+cat("####seqMerge ends\n\n")
+
+date()
+sink()
