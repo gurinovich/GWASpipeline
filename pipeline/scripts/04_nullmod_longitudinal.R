a6f86cfd994028f85b2bf96b6c6a7bedaa830373
ZeyuanSong <51000742+ZeyuanSong@users.noreply.github.com>
2020-09-28 00:39:29 -0400

    Update 04_nullmod_longitudinal.R

--- pipeline/scripts/04_nullmod_longitudinal.R
+++ pipeline/scripts/04_nullmod_longitudinal.R
@@ -1,28 +1,39 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
-phenotypes <- args[1]
-covariates <- unlist(strsplit(args[2], ","))
-model <- args[3]
-analysis.sample.id <- args[4]
-pc_df <- args[5]
-grm <- args[6]
-slope <- args[7]
+pheno.file <- args[1]
+phenotypes <- args[2]
+covariates <- unlist(strsplit(args[3], ","))
+model <- args[4]
+analysis.sample.id <- args[5]
+pc_df <- args[6]
+grm <- args[7]
+slope <- args[8]
 
 sink("nullmod_longitudinal.log", append=FALSE, split=TRUE)
 date()
 suppressPackageStartupMessages(library(data.table))
 suppressPackageStartupMessages(library(GMMAT))
+suppressPackageStartupMessages(library(dplyr))
 
-####PCA
+####Phenotype data
 pc.df <- readRDS(pc_df)
 analysis.sample.id <- readRDS(analysis.sample.id)
 pc.df <- pc.df[pc.df$sample.id%in%analysis.sample.id,]
+pc.df <- pc.df[, colnames(pc.df)%in%c("sample.id", paste0("PC",1:32))]
+pheno.dat <- read.csv(pheno.file, stringsAsFactors=F, header=T)
+pheno.dat$sample.id <- as.character(pheno.dat[,1])
+
+if(is.null(dim(pc.df))){
+    pheno.pc.dat <- pheno.dat[pheno.dat$sample.id%in%analysis.sample.id,]
+}else{
+    pheno.dat <- pheno.dat[pheno.dat$sample.id%in%analysis.sample.id,]
+    pheno.pc.dat <- left_join(pheno.dat, pc.df, by="sample.id")
+}
 
 ####covariance matrix from pcrelate output
 grm <- readRDS(grm)
 
 ####Mixed effect null model
-
 if(model=="linear"){
 	model.switch <- gaussian(link = "identity")
 }
@@ -39,13 +50,13 @@ fix.eff=formula(fix.eff)
 
 if(slope=="null"){
 	cat("\n####glmmkin starts\n")
-	nullmod <- glmmkin(fix.eff, data=pc.df, kins=as.matrix(grm),
+	nullmod <- glmmkin(fix.eff, data=pheno.pc.dat, kins=as.matrix(grm),
 	           random.slope=NULL, id="sample.id", family = model.switch)
 	cat("####glmmkin ends\n\n")
 
 }else{
 	cat("\n####glmmkin starts\n")
-	nullmod <- glmmkin(fix.eff, data=pc.df, kins=as.matrix(grm), 
+	nullmod <- glmmkin(fix.eff, data=pheno.pc.dat, kins=as.matrix(grm), 
 	           random.slope=slope, id="sample.id", family = model.switch)
 	cat("####glmmkin ends\n\n")
 
