01b485ed5bea58d9321be79e9217839b11a96de8
ZeyuanSong <51000742+ZeyuanSong@users.noreply.github.com>
2020-09-28 00:45:10 -0400

    Update 06_annovar_input.R

--- pipeline/scripts/06_annovar_input.R
+++ pipeline/scripts/06_annovar_input.R
@@ -23,7 +23,7 @@ snps.annovar <- snps %>%
   mutate(pos2 = pos, A1 = 0, A2 = 0) %>%
   select(chr, pos, pos2, A1, A2, snpID)
 
-write.table(snps.annovar, "top_snps_input.txt", quote = F, sep = "\t", row.names = F, col.names = F)
+write.table(snps.annovar, "top_snps_input.txt", sep = "\t", row.names = F, col.names = F)
 
 date()
 sink()
