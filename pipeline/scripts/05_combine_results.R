3b25dcf87df7a5ef36252fe737ac0ac6285dea51
ZeyuanSong <51000742+ZeyuanSong@users.noreply.github.com>
2020-09-29 18:11:46 -0400

    Update 05_combine_results.R

--- pipeline/scripts/05_combine_results.R
+++ pipeline/scripts/05_combine_results.R
@@ -14,7 +14,7 @@ result <- res
 #chr <- 2
 for (chr in 2:22) {
   res <- fread(paste0(path1, "chr_", chr, "_caf_annotated.csv"), stringsAsFactors=F, header=T)
-  result <- bind_rows(result, res)
+  result <- rbind(result, res)
 }
 
 fwrite(result, paste0("all_chr_caf_annotated.csv"), row.names = FALSE)
