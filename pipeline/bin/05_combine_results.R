717b1cc1f5dd18056502f3225ca4bd03ace53cb6
ZeyuanSong <51000742+ZeyuanSong@users.noreply.github.com>
2020-10-09 10:24:51 -0400

    Update 05_combine_results.R

--- pipeline/bin/05_combine_results.R
+++ pipeline/bin/05_combine_results.R
@@ -1,19 +1,14 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
-path1 <- args[1]
 
 sink('combine_results.log', append=FALSE, split=TRUE)
 date()
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(data.table))
 
-chr <- 1
-res <- fread(paste0(path1, "chr_", chr, "_caf_annotated.csv"), stringsAsFactors=F, header=T)
-result <- res
-
-#chr <- 2
-for (chr in 2:22) {
-  res <- fread(paste0(path1, "chr_", chr, "_caf_annotated.csv"), stringsAsFactors=F, header=T)
+result <- fread(args[1], stringsAsFactors=F, header=T)
+for (i in 2:length(args)) {
+  res <- fread(args[i], stringsAsFactors=F, header=T)
   result <- rbind(result, res)
 }
 
